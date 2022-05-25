082a1e09e45223beacb23c79675363c93710f7bc
Yutong Huang <yutong.huang@outlook.com>
2022-05-25 22:21:43 +0800

    feat: init commit

---
+++ init.lua
@@ -0,0 +1,148 @@
+-- easy access to config files
+local info = debug.getinfo(1,'S');
+local thisFile = string.sub(info.source, 2)
+
+vim.api.nvim_create_user_command(
+  'NvimConfig',
+  'e ' .. thisFile,
+  {bang = true}
+)
+
+vim.api.nvim_create_user_command(
+  'ReloadNvimConfig',
+  'source ' .. thisFile,
+  {bang = true}
+)
+-- load plugins
+require('plugins')
+
+-- plugin config start
+require('coc_config')
+
+require('onedark').setup {
+    style = 'darker',
+    toggle_style_key = ' ts',
+    code_style = {
+      functions = 'none',
+      keywords = 'none',
+      comments = 'none'
+    },
+}
+require('onedark').load()
+
+require('nvim-tree').setup {
+  view = { width = 50}
+}
+
+require('lualine').setup {
+  options = {
+    theme = 'onedark'
+  },
+}
+
+require('nvim_comment').setup {
+  comment_empty = false
+}
+
+-- require('nvim-treesitter.configs').setup{
+--   ensure_installed = {"cpp", "glsl"},
+--   highlight = {
+--     enable = true
+--   }
+-- }
+
+-- plugin config end
+
+vim.g.neoterm_shell = 'powershell.exe'
+vim.g.neoterm_default_mod = 'botright'
+vim.g.neoterm_autoinsert = 1
+vim.g.neoterm_size = 15
+
+local wk = require('whichkey_setup')
+wk.config{
+  default_keymap_settings = {
+    silent=true,
+    noremap=true,
+  },
+  default_mode = 'n',
+}
+local space_keymap = {
+  f = {
+    name = '+find',
+    f = {'<cmd>Telescope find_files<cr>', 'files'},
+    z = {'<cmd>Telescope current_buffer_fuzzy_find<cr>', 'in current buffer'},
+    c = {'<cmd>Telescope commands<cr>', 'commands'},
+    b = {'<cmd>Telescope buffers<cr>', 'buffers'}
+  },
+  b = {
+    name = '+buffers',
+    d = {'<cmd>bp<bar>sp<bar>bn<bar>bd<cr>', 'close current buffer but leaves window open'},
+    s = {'<C-^>', 'switch buffer'},
+  },
+  w = {
+    name = '+windows',
+    q = {'<cmd>wincmd q<cr>', 'close current window'},
+    h = {'<cmd>wincmd h<cr>', 'move to window to the left'},
+    j = {'<cmd>wincmd j<cr>', 'move to window to the up'},
+    k = {'<cmd>wincmd k<cr>', 'move to window to the bottom'},
+    l = {'<cmd>wincmd l<cr>', 'move to window to the right'},
+    w = {'<cmd>lua require("nvim-window").pick()<cr>', 'pick a window'},
+    v = {'<cmd>vsplit<cr>', 'split a window vertically'}
+  },
+  g = {
+    name = '+git',
+    s = {'<cmd>Git<cr>', 'open git window'},
+    b = {'<cmd>Git blame<cr>', 'show git blame'},
+    l = {
+      name = '+log',
+      p = {'<cmd>Git log --graph --oneline --decorate --abbrev-commit<cr>', 'show history graph'}
+    }
+  },
+}
+local commands = {
+  cmake = {
+    generateCompileCommands = [[ cmake -DCMAKE_TRY_COMPILE_TARGET_TYPE="STATIC_LIBRARY" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_MAKE_PROGRAM=nmake -S . -B compile_db_temp -G "Unix Makefiles" && copy compile_db_temp\compile_commands.json . && @RD /S /Q compile_db_temp ]]
+  }
+}
+local backslash_keymap = {
+  t = {'<cmd>Ttoggle<cr>', 'Toggle the neoterm window'},
+  f = {'<cmd>NvimTreeFocus<cr>', 'Toggle nvim tree'},
+  w = {'<cmd>lua require("nvim-window").pick()<cr>', 'pick a window'},
+  ['1'] = {'<cmd>buffer 1<cr>', 'open buffer 1'},
+  ['2'] = {'<cmd>buffer 2<cr>', 'open buffer 2'},
+  ['3'] = {'<cmd>buffer 3<cr>', 'open buffer 3'},
+  ['4'] = {'<cmd>buffer 4<cr>', 'open buffer 4'},
+  ['5'] = {'<cmd>buffer 5<cr>', 'open buffer 5'},
+  ['6'] = {'<cmd>buffer 6<cr>', 'open buffer 6'},
+  ['7'] = {'<cmd>buffer 7<cr>', 'open buffer 7'},
+  ['8'] = {'<cmd>buffer 8<cr>', 'open buffer 8'},
+  ['9'] = {'<cmd>buffer 9<cr>', 'open buffer 9'},
+  c = {
+    name = '+cmake options',
+    c = {'<cmd>!' .. commands.cmake.generateCompileCommands .. '<cr>', 'generate compile_commands.json'},
+    b = {'<cmd>!cmake -S . -B build && cmake --build build --config Release<cr>', 'build cmake project'}
+  },
+  o = {'<cmd>CocCommand clangd.switchSourceHeader<cr>', 'switch header source cpp'},
+  [';'] = {'<cmd>CocCommand clangd.inlayHints.toggle<cr>', 'toggle clangd inlayHints'}
+}
+wk.register_keymap(' ', space_keymap)
+wk.register_keymap('\\', backslash_keymap)
+
+vim.cmd [[ tnoremap <esc> <c-\><c-n> ]]
+vim.cmd [[ nnoremap <silent>Q <cmd>qa<cr> ]]
+vim.cmd [[ nnoremap <silent><c-s> <cmd>wa<cr> ]]
+vim.cmd [[ nmap <c-_> gcc]] -- comment line
+vim.cmd [[ vmap <c-_> gc]] -- comment block
+vim.cmd [[ colorscheme onedark]]
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.mouse = 'a'
+
+vim.cmd [[ autocmd! BufNewFile,BufRead *.vert,*.frag set ft=glslx ]]
---
+++ lua/plugins.lua
@@ -0,0 +1,100 @@
+vim.cmd [[packadd packer.nvim]]
+
+local info = debug.getinfo(1, 'S')
+local thisFile = string.sub(info.source, 2)
+
+vim.api.nvim_create_user_command(
+  'NvimPluginConfig',
+  'e ' .. thisFile,
+  {bang = true}
+)
+
+return require('packer').startup(function()
+
+	use 'wbthomason/packer.nvim'
+
+	use {'neoclide/coc.nvim', branch = 'release'}
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' }
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'liuchengxu/vim-which-key'
+  use {
+    'AckslD/nvim-whichkey-setup.lua',
+    requires = {'liuchengxu/vim-which-key'},
+  }
+
+  use{
+   'sonph/onehalf',
+   rtp = 'vim',
+   config = function()
+     -- vim.cmd 'colorscheme onehalfdark'
+   end
+  }
+
+  use {'kassio/neoterm'}
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  use {
+    'kdheepak/tabline.nvim',
+    config = function()
+      require'tabline'.setup {
+        -- Defaults configuration options
+        enable = true,
+        options = {
+        -- If lualine is installed tabline will use separators configured in lualine by default.
+        -- These options can be used to override those settings.
+          section_separators = {'', ''},
+          component_separators = {'', ''},
+          max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+          show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
+          show_devicons = true, -- this shows devicons in buffer section
+          show_bufnr = true, -- this appends [bufnr] to buffer section,
+          show_filename_only = false, -- shows base filename only instead of relative path in filename
+          modified_icon = "+ ", -- change the default modified icon
+          modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
+          show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+        }
+      }
+      vim.cmd[[
+        set guioptions-=e " Use showtabline in gui vim
+        set sessionoptions+=tabpages,globals " store tabpages and globals in session
+      ]]
+    end,
+    requires = { { 'hoob3rt/lualine.nvim', opt=true }, {'kyazdani42/nvim-web-devicons', opt = true} }
+  }
+
+  use "terrortylor/nvim-comment"
+
+  use "hyt589/nvim-window"
+
+  use 'tpope/vim-fugitive'
+
+  use 'navarasu/onedark.nvim'
+
+  use 'rafcamlet/coc-nvim-lua'
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  use {"ellisonleao/glow.nvim" }
+
+  use {"tikhomirov/vim-glsl"}
+
+end)
